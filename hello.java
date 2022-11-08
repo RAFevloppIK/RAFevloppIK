65f53eb5f01aff2aa3f83a6e78f65ba7a773ddea
Rafik Rou <rs375iwill@gmail.com>
2022-11-08 17:22:50 +0100

    fffd

---
+++ hello.java
@@ -0,0 +1,15 @@
+package TP1_package;
+
+
+public class hello {
+
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello, it's newwwwww !!!");
+		System.out.println("Hello 2");
+		System.out.println("Hello 333333");
+		
+	}
+
+}
